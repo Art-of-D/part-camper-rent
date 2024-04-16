fc917f666ee4fef39c7b6b73a0722870eed589eb
Artur Didur <didur.art@gmail.com>
2024-04-16 22:17:47 +0200

    general css

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { lazy, useEffect, useState } from 'react';
 import { CommonLayout } from './components/Layout/CommonLayout';
 import { Catalog } from './components/Catalog/Catalog';
 import { fetchAllData } from './mockapi/camperAPI';
+import style from './App.module.css';
 
 function App() {
   const [catalog, setCatalog] = useState([]);
@@ -16,13 +17,13 @@ function App() {
   }, []);
 
   return (
-    <>
+    <div className={style.commonWrapper}>
       <Routes>
         <Route path="/" element={<CommonLayout />}>
           <Route index element={<Catalog catalog={catalog} />} />
         </Route>
       </Routes>
-    </>
+    </div>
   );
 }
 
